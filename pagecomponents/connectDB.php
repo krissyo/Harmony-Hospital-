ed637e81406e811995b8424b4a14d2e89e192657
Kira Jamison <kira.jamison@connect.qut.edu.au>
2014-04-29 21:34:22 +1000

    db include file

--- pagecomponents/connectDB.php
+++ pagecomponents/connectDB.php
@@ -3,7 +3,7 @@
 // DB connection from one place in our application
 //mysqli_connect(host,username,password,dbname);
 //$con=mysqli_connect("mysql.Firelabs.com.au","inb201harmony","6wxPSiPp","inb201harmony");
-$con = mysqli_connect("http://trustinblack.com/phpmyadmin/","harmony", "admin2014", "inb201harmony");
+$con = mysqli_connect("http://trustinblack.com/phpmyadmin/","trustinb_harmony", "admin2014", "inb201harmony");
 
 if (!$con) {
   die('Could not connect: ' . mysqli_error($con));
