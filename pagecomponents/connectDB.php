235dcb86570c29705f320f1aaffab271909748dd
Kira Jamison <kira.jamison@connect.qut.edu.au>
2014-04-30 09:55:34 +1000

    Successful mod of connectDB.php

--- pagecomponents/connectDB.php
+++ pagecomponents/connectDB.php
@@ -4,7 +4,7 @@
 //mysqli_connect(host,username,password,dbname);
 //$con=mysqli_connect("mysql.Firelabs.com.au","inb201harmony","6wxPSiPp","inb201harmony");
 //$con = mysqli_connect("http://trustinblack.com/phpmyadmin/","trustinb_harmony", "inb201", "trustinb_harmonyhospital");
-$con = mysqli_connect('http://trustinblack.com/phpmyadmin/', 'trustinb_harmony', 'inb201', 'trustinb_harmonyhospital');
+$con = mysqli_connect('localhost', 'trustinb_harmony', 'inb201', 'trustinb_harmonyhospital');
 if (!$con) {
   die('Could not connect: ' . mysqli_error($con));
 }
