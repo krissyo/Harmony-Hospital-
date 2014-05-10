977f18ed01881a479f8e583e5258690b93ef9d58
Jesse Cunningham - Creech <trustinblack@Jesses-MacBook-Pro.local>
2014-05-10 20:31:01 +1000

    Harmony_invoice.php now pulling from DB

--- Harmony_invoice.php
+++ Harmony_invoice.php
@@ -18,7 +18,7 @@
 			$this->Cell(110);
 
 			//Title
-			$this->Cell(88,8,'DOCUMENT TITLE',0,2,'C');
+			$this->Cell(88,8,'INVOICE',0,2,'C');
 
 			//Line Break
 			$this->Ln(2);
@@ -35,7 +35,7 @@
 			$this->Cell(110);
 			$this->SetFont('Helvetica','B',15);
 			$this->SetLineWidth(1);
-			$this->Cell(88,8,'DOCUMENT NUMBER',1,2,'C');
+			$this->Cell(88,8,'INVOICE NUMBER',1,2,'C');
 
 			//Line Break
 			$this->Ln(0);
@@ -48,13 +48,33 @@
 			//Move to the right
 			$this->Cell(110);
 
+			//get current date
+			$currentDate = new DateTime();
+			$currentDate = $currentDate->format('d-m-Y');
+
 			//output Date
 			$this->SetFont('Helvetica','B',12);
-			$this->Cell(88,8,'20/05/2014',0,2,'C');
+			$this->Cell(88,8,$currentDate,0,2,'C');
 			$this->Ln(2);
 		}
 
-		function PatientDetails(){			
+		function PatientDetails(){
+
+			//Connect to database
+			include ('pagecomponents/connectDB.php');
+
+			if (!isSet($_SESSION['AdmissionId'])) {
+			$_SESSION['AdmissionId'] = 1;
+			}	
+
+			//Select records
+			$sql = 'SELECT patient_id,first_name, middle_name, last_name,
+			address_line, postcode
+			FROM patient_details 
+			WHERE patient_id = 1';
+
+			$result=mysqli_query($con,$sql);
+			
 
 			// Set Title Font
 			$this->SetTextColor(165,165,167);
@@ -64,20 +84,27 @@
 			//$this->Cell(0);
 
 			//Title
-			$this->Cell(88,8,'PATIENT DETAILS',0,2,'L');
+			$this->Cell(88,8,$row,0,2,'L');
 
-			// Patient details Varaibles
-			$patientName = "Jesse Cunningham - Creech \n";
-			$addressLn1 = "34 Mossman Street, Newmarket \n";
-			$addressLn2 = "Qld, 4051";
-			$patientDetails = $patientName.$addressLn1.$addressLn2;
 
-			//Create details border Box and input patient details
-			$this->SetFont('Helvetica','',12);
-			$this->SetLineWidth(1);
-			$this->SetDrawColor(135,186,214);			
-			$this->MultiCell(88,7,$patientDetails,1,'L');
-			
+			// Patient details Variables Set
+			if($result){
+				$row = mysqli_fetch_array($result);
+				$patientId = "Patient Id:".$row['patient_id'];
+				$space = "\n";
+				$patientName = $row['first_name']." ".$row['middle_name']." ".$row['last_name'];
+				$addressLn1 = $row['address_line']." ".$row['postcode'];
+				$patientDetails = $patientId.$space.$patientName.$space.$addressLn1;	
+
+				//Create details border Box and input patient details
+				$this->SetFont('Helvetica','',12);
+				$this->SetLineWidth(1);
+				$this->SetDrawColor(135,186,214);			
+				$this->MultiCell(88,7,$patientDetails,1,'L');			
+			}
+
+						
+			include('pagecomponents/closeConnection.php');
 		}
 	}
 
