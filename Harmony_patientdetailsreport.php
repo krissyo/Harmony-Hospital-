0abd8ea5318f20fef89db9db8cc8898802043e1e
Jesse Cunningham - Creech <trustinblack@Jesses-MacBook-Pro.local>
2014-05-11 23:01:18 +1000

    Harmony_patientdetailsreport.php formatted and linking to DB

--- Harmony_patientdetailsreport.php
+++ Harmony_patientdetailsreport.php
@@ -12,33 +12,18 @@
 
 			// Set Document Title Font
 			$this->SetTextColor(165,165,167);
-			$this->SetFont('Helvetica','B',28);
+			$this->SetFont('Helvetica','B',20);
 
 			//Move to the right
-			$this->Cell(110);
+			$this->Cell(95);
 
 			//Title
-			$this->Cell(88,8,'INVOICE',0,2,'C');
+			$this->Cell(88,8,'PATIENT DETAILS REPORT',0,2,'L');
 
 			//Line Break
 			$this->Ln(2);
 
-			////--------Document Number
-
-			// Set Border Colour
-			$this->SetDrawColor(165,165,167);
-
-			// Set Text Colour
-			$this->SetTextColor(135,186,214);
-
-			//Move to the right
-			$this->Cell(100);
-			$this->SetFont('Helvetica','B',15);
-			$this->SetLineWidth(1);
-			$this->Cell(88,8,'INVOICE NUMBER',1,2,'C');
-
-			//Line Break
-			$this->Ln(0);
+			
 
 			////--------Document Date
 
@@ -46,15 +31,16 @@
 			$this->SetTextColor(165,165,167);
 			
 			//Move to the right
-			$this->Cell(110);
+			$this->Cell(100);
 
 			//get current date
 			$currentDate = new DateTime();
 			$currentDate = $currentDate->format('d-m-Y');
 
-			//output Date
+			//output Report Date
+			$this->SetTextColor(135,186,214);
 			$this->SetFont('Helvetica','B',12);
-			$this->Cell(88,8,$currentDate,0,2,'C');
+			$this->Cell(88,8,"REPORT DATE ".$currentDate,0,2,'C');
 			$this->Ln(2);
 		}
 
@@ -70,9 +56,10 @@
 
 			//Select records
 			$sql = 'SELECT patient_id,first_name, middle_name, last_name,
-			address_line, postcode
+			address_line, postcode, date_of_birth, date_of_death, gender,
+			medicare_number, medicare_expiry_date
 			FROM patient_details 
-			WHERE patient_id = 1';
+			WHERE patient_id = 3';
 
 			$result=mysqli_query($con,$sql);
 			
@@ -94,11 +81,18 @@
 			// Patient details Variables Set
 			if($result){
 				$row = mysqli_fetch_array($result);
-				$patientId = "Patient Id:".$row['patient_id'];
+				$patientId = "PATIENT ID: ".$row['patient_id'];
 				$space = "\n";
 				$patientName = $row['first_name']." ".$row['middle_name']." ".$row['last_name'];
+				$DOB= "DOB:".$row['date_of_birth'];
+				if($row['date_of_death']){
+					$DOD = " DOD:".$row['date_of_death'];
+				}
 				$addressLn1 = $row['address_line']." ".$row['postcode'];
-				$patientDetails = $patientId.$space.$patientName.$space.$addressLn1;	
+				$medicareNumber = "Medicare number: ".$row['medicare_number'];
+				$medicareExp = "Medicare Expiry Date: ".$row['medicare_expiry_date'];
+				$patientDetails = $patientId.$space.$patientName.$space.$addressLn1.$space.$DOB.$DOD.
+				$space.$medicareNumber.$space.$medicareExp;	
 
 				//Create details border Box and input patient details
 				$this->SetFont('Helvetica','',12);
@@ -107,28 +101,68 @@
 				$this->MultiCell(88,7,$patientDetails,1,'L');			
 			}						
 			include('pagecomponents/closeConnection.php');
+			//$this->Ln(10);
+		}
+
+		function CarerDetails(){
+
+			//Connect to database
+			include ('pagecomponents/connectDB.php');
+
+			if (!isSet($_SESSION['AdmissionId'])) {
+			$_SESSION['AdmissionId'] = 1;
+			}	
+
+			//Select records
+			$sql2 = 'SELECT carer1_name, carer1_address, carer1_phone_number, carer2_name, carer2_address, carer2_phone_number,
+			address_line, postcode
+			FROM patient_details 
+			WHERE patient_id = 1';
+
+			$result=mysqli_query($con,$sql2);
+			
+			//Set the draw Text colour to black
+			$this->SetTextColor(0,0,0);
+
+
+			// carer details Variables Set
+			if($result){
+				$row = mysqli_fetch_array($result);
+				$space = "\n";
+				$carerName = "CARER 1: ".$row['carer1_name'];
+				$carerPhone= "PH: ".$row['carer1_phone_number'];
+				$address1 = $row['carer1_address']." ";
+				$carer2Name = "CARER 2: ".$row['carer2_name'];
+				$carer2Phone = "PH: ".$row['carer2_phone_number'];
+				$address2 = $row['carer2_address']." ";
+				$patientDetails = $carerName.$space.$carerPhone.$space.$address1.$space.
+				$carer2Name.$space.$carer2Phone.$space.$address2;	
+
+				//Create details border Box and input patient details
+				$this->SetFont('Helvetica','',12);
+				$this->SetLineWidth(1);
+				$this->SetDrawColor(165,165,167);			
+				$this->MultiCell(88,7,$patientDetails,1,'L');			
+			}						
+			include('pagecomponents/closeConnection.php');
 			$this->Ln(10);
 		}
 
 		// Load Table data
-		function LoadData($file)
+		function LoadAdmData($file)
 		{
 			//Connect to database
 			include ('pagecomponents/connectDB.php');
 
 			// Testing with AdmissionId set to 1
-			if (!isSet($_SESSION['AdmissionId'])) {
-			$_SESSION['AdmissionId'] = 1;
+			if (!isSet($_SESSION['PatientId'])) {
+			$_SESSION['PatientId'] = 1;
 			}
 	
-			//Select records
-			$sql = 	'SELECT service_start_date, service_end_date,
-			procedure_description, procedure_fee, 
-			medicare_rebate
-			FROM patient_services 
-			INNER JOIN procedure_listing
-			ON patient_services.procedure_id = procedure_listing.procedure_id
-			WHERE admission_id = ' . $_SESSION["AdmissionId"] . ' ORDER BY service_start_date, service_end_date';
+			//Select Admission records
+			$sql = 	'SELECT admission_id, admission_date, notes
+			FROM admissions 
+			WHERE patient_id = ' . $_SESSION["PatientId"];
 			
 			$data=mysqli_query($con,$sql);
 	
@@ -142,11 +176,8 @@
 
 			
 
-		function invoiceTable($header, $data, $totalsHeader){
-			// Special conversion used to convert 
-			// date difference into days
-			$CONVERSION = 86400;
-	
+		function AdmissionTable($admheader, $admdata){
+
     		// Colors, line width and bold font
     		$this->SetFillColor(255,255,255);
     		$this->SetTextColor(135,186,214);
@@ -156,84 +187,163 @@
 
     		// Header Line of table
     		// Set Column Widths
-    		$w = array(47, 32, 42, 42, 27);
+    		
     		// Header
-    		for($i=0;$i<count($header);$i++)
-        	$this->Cell($w[$i],10,$header[$i],1,0,'C',true);
+    		$this->Cell(190,10,"PATIENT ADMISSIONS HISTORY",1,0,'C',true);
     		$this->Ln();
+    		$w = array(40, 40, 110);
+    		$this->SetTextColor(165,165,167);
+    		for($i=0; $i<count($admheader);$i++)
+    			$this->Cell($w[$i],10,$admheader[$i],1,0,'C',true);    			
+    		$this->Ln();
+
 
     		// Color and font restoration
     		$this->SetFillColor(224,235,255);
     		$this->SetTextColor(0);
     		$this->SetFont('');
 
-   			// Data
-    		$fill = false;
-			$feeTotal = 0;
-			$rebateTotal = 0;
 	
-    		while($row = mysqli_fetch_array($data)) {
+    		while($row = mysqli_fetch_array($admdata)) {
 	
-				// Calculate days if the Service is open-dated
-				if (is_null($row['service_end_date'])) {
-						
-					// Calculate fees as at today's date
-					$row['service_end_date'] = date('Y-m-d');
-					}
-			
-				if ($row['service_start_date'] == $row['service_end_date']) {		
-					$days = 1;			
-					} else {
-					$days = Round(ABS(strToTime($row['service_start_date'])-strToTime($row['service_end_date']))/$CONVERSION);
-					}
-			
 				// Populate Data
-        		$this->Cell($w[0],10, $row['procedure_description'],'LR',0,'L',$fill);
-				$startDate = date("d/m/Y", strToTime($row['service_start_date']));
-       	 		$this->Cell($w[1],10, $startDate,'LR',0,'L',$fill);
-        		$this->Cell($w[2],10, '$' . number_format($row['procedure_fee'], 2),'LR',0,'R',$fill);
-        		$this->Cell($w[3],10, '$' . number_format($row['medicare_rebate'], 2),'LR',0,'R',$fill);
-				$this->Cell($w[4],10, $days,'LR',0,'R',$fill);
-        		$this->Ln();
-        		$fill = !$fill;
-			
-				// Variables to calculate total Cost
-				$feeTotal += $row['procedure_fee'] * $days;
-				$rebateTotal += $row['medicare_rebate'] * $days;
-    			}
+				$notes = $row['notes'];
+        		$notesLength = strlen($notes);
+        		// provide for long admission notes
+				if($notesLength>40){
+					$x = $this->x;
+					$y = $this->y;	
+					$space = "\n";
+					$admission_id = $row['admission_id'].$space.$space;
+					$admission_date = $row['admission_date'].$space.$space;	
+        			$this->MultiCell($w[0],10,$admission_id,1,'L',$fill);
+        			$this->SetXY($x + $w[0], $y);
+        			$this->MultiCell($w[1],10,$admission_date,1,'L',$fill);
+        			$this->SetXY($x + $w[0] + $w[1], $y);        		
+        			$this->MultiCell($w[2],10,$notes,1,'L',$fill);        			
+        			$fill = !$fill;
+        		}
+        		else{
+        			$this->Cell($w[0],10,$row['admission_id'],1,0,'L',$fill);
+        			$this->Cell($w[1],10,$row['admission_date'],1,0,'L',$fill);
+        			$this->Cell($w[2],10,$notes,1,0,'L',$fill);
+        			$fill = !$fill;
+        		}       		
+
+        	}
+        	$this->Ln();
+
+
+			    		// Closing line
+    					$this->Cell(array_sum($w),0,'','T');
+    					$this->Ln(10);
+				
+    	}
+
+    	// Load Table data
+		function LoadMedData($file)
+		{
+			//Connect to database
+			include ('pagecomponents/connectDB.php');
+
+			// Testing with AdmissionId set to 1
+			if (!isSet($_SESSION['PatientId'])) {
+			$_SESSION['PatientId'] = 2;
+			}
 	
-    		// Closing line
-    		$this->Cell(array_sum($w),0,'','T');
-    		$this->Ln(10);
+			//Select Admission records
+			$sql = 	'SELECT doctors_notes, current_medication, allergies, conditions,
+			height, weight
+			FROM medical_history 
+			WHERE patient_id = ' . $_SESSION["PatientId"];
+			
+			$data=mysqli_query($con,$sql);
+	
+			if (!$data) {
+			return 'No data to display.';
+			} else {
+			return $data;
+			}
+			include ('pagecomponents/closeConnection.php');
+			}
+
+    	function MedicalHistoryTable($medheader, $meddata){
 
     		// Colors, line width and bold font
     		$this->SetFillColor(255,255,255);
     		$this->SetTextColor(135,186,214);
     		$this->SetDrawColor(165,165,167);	
     		$this->SetLineWidth(.8);
-   			$this->SetFont('','B',12);
+   			$this->SetFont('','B');
 
-   			// Header Line of table
+    		// Header Line of table
     		// Set Column Widths
-    		$w2 = array(53.3,53.3,83.3);
+    		
     		// Header
-    		for($i=0;$i<count($totalsHeader);$i++)
-        	$this->Cell($w2[$i],10,$totalsHeader[$i],1,0,'C',true);
+    		$this->Cell(190,10,"PATIENT MEDICAL HISTORY",1,0,'C',true);
+    		$this->Ln();
+    		$w = array(63.3, 63.3, 63.3);
+    		$this->SetTextColor(165,165,167);
+    		for($i=0; $i<count($medheader);$i++)
+    			$this->Cell($w[$i],10,$medheader[$i],1,0,'C',true);    			
     		$this->Ln();
 
-    		$this->SetTextColor(0,0,0);
 
-			// Display the Totals
-			$this->SetFont('','');
-			$this->Cell($w2[0],10,'$'.number_format($feeTotal, 2),1,0,'C');
-			$this->Cell($w2[1],10,'$'.number_format($rebateTotal, 2),1,0,'C');
-			// Calculate Gap Total
-			$this->SetFont('','B');
-			$gapTotal = $feeTotal - $rebateTotal;
-			$this->Cell($w2[2],10,'$'.number_format($gapTotal, 2),1,0,'C');
-			$this->Ln();
+    		// Color and font restoration
+    		$this->SetFillColor(224,235,255);
+    		$this->SetTextColor(0);
+    		$this->SetFont('');
+
+   		
 	
-		}
+    		while($row = mysqli_fetch_array($meddata)) {
+	
+				// Populate Data
+				$this->Cell($w[0],10,$row['height'],1,0,'c',false);
+				$this->Cell($w[1],10,$row['weight'],1,0,'c',false);
+				$this->Cell($w[2],10,$row['allergies'],1,0,'c',false);
+				$this->Ln();
+				$this->Cell($w[0],10,'Doctors Notes',1,0,'c',true);
+				$this->Ln();
+				$w2 = array(30,160);
+				$notes = $row['doctors_notes'];
+        		$notesLength = strlen($notes);
+        		// provide for long admission notes
+				if($notesLength>40){
+					$this->MultiCell(190,10,$row['doctors_notes'],1,'L',$fill);
+					$this->Ln();        			  			
+        			$fill = !$fill;
+        		}
+        		else{
+        			$this->Cell(190,10,$row['doctors_notes'],1,0,'L',$fill);
+        			$this->Ln();
+        			$fill = !$fill;
+        		}
+        		if($row['nurse_notes']){
+        			$this->Cell($w[0],10,'Nurses Notes',1,0,'c',true);
+					$this->Ln();
+        			$notes = $row['nurses_notes'];
+        			$notesLength = strlen($notes);
+        			// provide for long admission notes
+					if($notesLength>40){
+						$this->MultiCell(190,10,$row['nurses_notes'],1,'L',$fill);        			  			
+        				$fill = !$fill;
+        				}
+        			else{
+        				$this->Cell(190,10,$row['nurses_notes'],1,0,'L',$fill);
+        				$fill = !$fill;
+        				}
+        		}            		
+
+        	}
+        	$this->Ln();
+
+
+			    		// Closing line
+    					//$this->Cell(array_sum($w),0,'','T');
+    					//$this->Ln(10);
+				
+    	}
 
 		// Page footer
 		function Footer()
@@ -253,10 +363,16 @@
 	$pdf->AliasNbPages();
 	$pdf->AddPage();
 	$pdf->PatientDetails();
-	$data = $pdf->LoadData($file);
-	$header = array('SERVICE', 'DATE', 'FEE', 'REBATE', 'DAYS');
-	$totalsHeader = array('TOTAL FEES','TOTAL REBATE','TOTAL GAP AMOUNT PAYABLE');
-	$pdf->invoiceTable($header, $data, $totalsHeader);
+	$pdf->SetXY(113,38);
+	$pdf->CarerDetails();
+	$pdf->SetXY(10,90);
+	$admdata = $pdf->LoadAdmData($file);
+	$admheader = array('Admission Id', 'Admission Date', 'Admission Notes');
+	
+	$pdf->AdmissionTable($admheader, $admdata);
+	$meddata = $pdf->LoadMedData($file);
+	$medheader = array('Height', 'Weight','Allergies');
+	$pdf->MedicalHistoryTable($medheader, $meddata);
 	$pdf->Output();
 
 ?>
