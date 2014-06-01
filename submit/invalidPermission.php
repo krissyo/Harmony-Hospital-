37bfbe353abcce97b3ec3df5eea3cb6da0d18a3d
Jesse Cunningham - Creech <trustinblack@Jesses-MacBook-Pro.local>
2014-06-01 14:49:01 +1000

    invalid permissions page added

---
+++ submit/invalidPermission.php
@@ -0,0 +1,19 @@
+<?php
+	// @author: Jesse Cunningham-Creech 05420687
+        session_start();
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+<link type="text/css" rel="stylesheet" href="http://trustinblack.com/harmonyhospital/globalstyle.css" > 
+<title> ERROR</title>
+</head>
+<body>
+<div class="gradient"></div>
+<div class="wrapper">
+<a href="http://trustinblack.com/harmonyhospital/index.php"><img class="centre" src="http://trustinblack.com/harmonyhospital/images/harmony_logo_blue_locked.png" width="490" height="520"></a>
+<div class="centre"><h3><br><br> YOU HAVE REQUESTED A RESOURCE THAT <br> YOU DONT HAVE ACCESS TOO <br></h3><h3><br> please contact your system admin</h4></div>
+</div>
+</body>
+</html>
