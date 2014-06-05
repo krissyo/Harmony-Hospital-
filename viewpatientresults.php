270e1c1b4778a48e6ba07bd98356704c3e72b0cf
Jesse Cunningham - Creech <trustinblack@Jesses-MacBook-Pro.local>
2014-06-05 12:19:18 +1000

    view patients is now formatted

--- viewpatientresults.php
+++ viewpatientresults.php
@@ -16,7 +16,11 @@ $_SESSION['passingID'] = $patientid; // patient id
 			<form id="testresults" action="" method="post" enctype="multipart/form-data">
                 
                 <input type="hidden" >
-			<table><h3><th colspan="2" class="userdetails">
+			<table style="width: 600px">
+            <colgroup>
+             <col span="1" style="width: 50%;">
+            <col span="1" style="width: 50%;">
+                <h3><th colspan="2" class="userdetails">
                 
                 <?php
                     $sql=" SELECT first_name, last_name FROM patient_details WHERE patient_id=" . $patientid .'';  // Name of Patient
@@ -32,29 +36,31 @@ $_SESSION['passingID'] = $patientid; // patient id
                             require_once('pagecomponents/connectDB.php');
 							$sql="SELECT medical_image, test_notes FROM clinical_history_detail WHERE patient_id=" . $patientid .''; // Patient Image
                             $result=mysqli_query($con,$sql);
-                            
+                            $count = 1;
                             while($row = mysqli_fetch_array($result)){
-                                if (empty($row['medical_image']))
+                                if ($row['medical_image'] != null)
                                 {
-                                    echo 'No image uploaded';
-                                }
-                                else
-                                {
-                                    echo "<tr><td>Image:</td>";
-                                    echo "<td>";
-                                    echo "<div>";
+                                    echo '<tr colspan="2" ><td><b> test result '.$count.'</b></td>';
+                                    echo "<tr><td>";
+                                   
                                     echo '<a href="/harmonyhospital/testresultsimg/'.$row['medical_image'].'">';
-                                    echo '<img src="/harmonyhospital/testresultsimg/'.$row['medical_image'].'" style ="height:50%; width:50%; max-width:400px; margin:0; margin-left:auto; margin-right:auto;">';
+                                    echo '<img src="/harmonyhospital/testresultsimg/'.$row['medical_image'].'" style ="height:50%; width:50%; max-width:4   00px; margin:0; margin-left:auto; margin-right:auto;">';
                                     echo '</a>';
-                                    echo "</div>";
+                                   
                                     echo "</td>";
                                     echo "<td>";
-                                    echo '<p>'.$row['test_notes'].'</p>';
+                                    echo $row['test_notes'];
                                     
                                     echo "</td>";
                                     echo "</tr>";
-
                 	           }
+                               else{
+                                echo '<tr colspan="2" ><td><b> test result '.$count.'</b></td>';
+                                echo '<tr><td></td><td>'.$row['test_notes'].'</td>';
+                                echo "</tr>";
+
+                               }
+                               $count ++;
                             }
 						  ?>
 
